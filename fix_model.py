073e7c843f7448ccef7b890ec183864afc799248
UltralyticsAssistant <web@ultralytics.com>
2026-04-17 04:26:46 +0000

    Auto-format by https://ultralytics.com/actions

--- fix_model.py
+++ fix_model.py
@@ -1,19 +1,17 @@
-import torch
 import pathlib
+
+import torch
 
 # FIX path Linux -> Windows
 pathlib.PosixPath = pathlib.WindowsPath
 
 # IZINKAN model YOLOv5
 from models.yolo import DetectionModel
+
 torch.serialization.add_safe_globals([DetectionModel])
 
 # load model (trusted source)
-model = torch.load(
-    "weights/best.pt",
-    map_location="cpu",
-    weights_only=False
-)
+model = torch.load("weights/best.pt", map_location="cpu", weights_only=False)
 
 # save ulang versi Windows-safe
 torch.save(model, "weights/best_fixed.pt")
